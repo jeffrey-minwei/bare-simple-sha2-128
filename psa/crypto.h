0a9e109b7acafd1dce1af4b8acfcc985e4c33911
jeffrey-minwei <jeffrey.minwei@gmail.com>
2025-10-06 02:08:55 +0800

    PSA_ALG_SHA_256

--- psa/crypto.h
+++ psa/crypto.h
@@ -12,6 +12,16 @@ typedef int32_t psa_status_t;
 #define PSA_SUCCESS ((psa_status_t)0)
 
 /**
+ * See https://arm-software.github.io/psa-api/crypto/1.1/api/ops/algorithms.html#c.psa_algorithm_t
+ */
+typedef uint32_t psa_algorithm_t;
+
+/**
+ * See https://arm-software.github.io/psa-api/crypto/1.1/api/ops/hashes.html#c.PSA_ALG_SHA_256
+ */
+#define PSA_ALG_SHA_256 ((psa_algorithm_t)0x02000009)
+
+/**
  * See https://arm-software.github.io/psa-api/crypto/1.1/api/library/library.html#c.psa_crypto_init
  */
 psa_status_t psa_crypto_init();
