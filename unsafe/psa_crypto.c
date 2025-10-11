6ae2b8ee4a7eb793ff9309cf8745fc24bdaf4072
jeffrey-minwei <jeffrey.minwei@gmail.com>
2025-10-12 06:14:54 +0800

    crypto_sign_open #ifdef X86

--- unsafe/psa_crypto.c
+++ unsafe/psa_crypto.c
@@ -268,7 +268,9 @@ int crypto_sign_open(unsigned char *m, unsigned long long *mlen,
 
     // See See https://github.com/sphincs/sphincsplus/blob/consistent-basew/ref/sign.c#L269
     if (smlen < SPX_BYTES) {
+        #ifdef X86
         printf("crypto_sign_open: smlen %d (SPX_BYTES %d)\n", smlen, SPX_BYTES);
+        #endif
         return -1;  // malformed input
     }
 
